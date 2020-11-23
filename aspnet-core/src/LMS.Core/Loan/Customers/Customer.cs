653732d8dc035fab9bc32b6592e83a8fc8c02de5
Ali.Ali <ai_essa_ali@outlook.com>
2020-11-23 09:04:25 -0800

    customer with nationailty

---
+++ aspnet-core/src/LMS.Core/Loan/Customers/Customer.cs
@@ -0,0 +1,35 @@
+﻿using Abp.Domain.Entities.Auditing;
+using LMS.Loan.Indexes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace LMS.Loan.Customers
+{
+    public class Customer : FullAuditedEntity
+    {
+        public Customer()
+        {
+            //Cases = new List<Case>();
+        }
+        public string Name { get; set; }
+        public string ForeignName { get; set; }
+        public string Number { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string OfficePhone { get; set; }
+        public string HomePhone { get; set; }
+        public string MobilePhone { get; set; }
+        public string OtherPhone { get; set; }
+        public string PassportNumber { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Note { get; set; }
+        public int NationalityId { get; set; }
+        [ForeignKey("NationalityId")]
+        public Nationality Nationality { get; set; }
+
+    }
+}
---
+++ aspnet-core/src/LMS.Core/Loan/Indexes/Nationality.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LMS.Loan.Indexes
+{
+    public class Nationality : FullAuditedEntity
+    {
+        [MaxLength(LMSConsts.MaxIndexStringLength)]
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+}
